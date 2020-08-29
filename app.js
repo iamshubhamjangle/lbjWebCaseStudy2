ffb2c695a1bfd8e272cadc2b2577883497da1ca5
imshubham <jangleshubham@ymail.com>
2020-08-29 12:47:46 +0530

    improved overall style

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var app = express();
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 const csv = require('csv-parser');
 const fs = require('fs');
-var csvWriter = require('csv-write-stream')
-var writer = csvWriter()
+var csvWriter = require('csv-write-stream');
+var writer = csvWriter();
 
 const path = "D:/Documents/Study material/LTI/lbj_webStudy2/public/students.csv";
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs"); //help to avoid .ejs HTML page
 app.use(express.static("public"));
 
@@ -33,7 +33,7 @@ app.get("/viewStudent", function(req, res){
   		console.log(row);
   	})
   	.on('end', () => {
-    	//console.log('CSV file successfully processed');
+    	console.log('CSV file successfully processed');
   	});
   	res.render("viewStudent");
 });
@@ -41,13 +41,28 @@ app.get("/viewStudent", function(req, res){
 app.post("/addNewStudent", function(req, res){
 	var sid = req.body.sid;
 	var sname = req.body.sname;
-	console.log("\nData received from input: " + req.body.sid + " " + req.body.sname);
+	var sgender =req.body.sgender;
+	var sdob = req.body.sdob;
+	var scity = req.body.scity;
+	var sstate = req.body.sstate;
+	var semail = req.body.semail;
+	var squalification = req.body.squalification;
+	var sstream = req.body.sstream;
+
+	console.log(req.body);
 
 	var writer = csvWriter({sendHeaders: false})
 	writer.pipe(fs.createWriteStream(path, {flags: 'a'}))
 	writer.write({
-		Student_Id: sid, 
-		name: sname
+		"Student ID": sid, 
+		"Name": sname,
+		"Gender": sgender,
+		"DOB": sdob,
+		"City": scity,
+		"State": sstate,
+		"Email": semail,
+		"Qualification": squalification,
+		"Stream": sstream
 	})
 	writer.end()
 
